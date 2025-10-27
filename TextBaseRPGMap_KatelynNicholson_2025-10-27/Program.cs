e7066905e134cac682c1344ee79676a23f395f9a
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-27 10:08:59 -0300

    Initial commit

---
+++ TextBaseRPGMap_KatelynNicholson_2025-10-27/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBaseRPGMap_KatelynNicholson_2025_10_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TextBaseRPGMap_KatelynNicholson_2025-10-27/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TextBaseRPGMap_KatelynNicholson_2025-10-27")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NSCC")]
+[assembly: AssemblyProduct("TextBaseRPGMap_KatelynNicholson_2025-10-27")]
+[assembly: AssemblyCopyright("Copyright © NSCC 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f1908462-73a9-4c51-8579-575698839c86")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
